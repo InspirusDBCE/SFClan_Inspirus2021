f10315ca6362e6465a8fb26aaf66699357f66ebc
Neil Botelho <neil.botelho321@gmail.com>
2021-10-30 02:46:53 +0530

    Add registration endpoints

--- backend/app.py
+++ backend/app.py
@@ -25,10 +25,9 @@ db = MongoEngine()
 db.init_app(app)
 
 bcrypt = Bcrypt(app)   
-
+LATEST_BID=0
 # CANDIDATE  =bcrypt.gensalt()
 # COOKIE_NAME='bus-session'
-
 
 
 
@@ -43,6 +42,46 @@ class BusManager(db.Document):
                 "password": self.password,
                 "manager_id": self.id
                 }
+
+class Bus(db.Document):
+    reg=db.StringField()
+    bid=db.IntField()
+    sc_lat=db.ListField(db.FloatField())
+    sc_long=db.ListField(db.FloatField())
+    sc_pid=db.ListField(db.StringField())
+    sc_time=db.ListField(db.StringField())
+    sc_name=db.ListField(db.StringField())
+    curr_lat=db.FloatField()
+    curr_long=db.FloatField()
+    next_dest=db.StringField()
+
+    def to_json(self):
+        sched=[]
+        for i in range(len(sc_lat)):
+            sched.append({
+                "lat":self.sc_lat[i],
+                "long":self.sc_long[i],
+                "pid":self.sc_pid[i],
+                "name":self.sc_name[i],
+                "time":self.sc_time[i],
+
+                })
+        return {
+                "reg":self.ref ,
+                "sc":sched,
+                 "number": self.phoneno,
+                "password": self.password,
+                "manager_id": self.id
+                }
+
+def get_bid():
+    global LATEST_BID    
+    for bus in Bus.objects():
+        print(bus,bus.bid)
+        if LATEST_BID<bus.bid:
+            LATEST_BID=bus.bid
+    return LATEST_BID
+get_bid()
 
 def generate_random_string():
    return '%030x' % random.randrange(16**30)
@@ -101,6 +140,47 @@ def okok():
 @token_required
 def authen():
     return jsonify({"message" : "you're authenticated bitch"})
+
+    
+
+@app.route('/bus',methods=['POST'])
+@token_required
+def new_bus():
+    global LATEST_BID
+    token = request.headers['x-access-token']
+    phone=jwt.decode(token, app.config['SECRET_KEY'], algorithms = 'HS256',options={"verify_signature": False})['phone']
+    current_user=BusManager.objects(phone=phone).first()
+    print(current_user) 
+    newBusIds=current_user.busIds
+    print(newBusIds)
+    newBusIds.append(LATEST_BID+1)
+
+    list(set(newBusIds))
+
+
+    current_user.update(busIds=newBusIds)
+    args=request.get_json()
+    sched=args["schedule"]
+    sc_lat=[]
+    sc_long=[]
+    sc_pid=[]
+    sc_name=[]
+    sc_time=[]
+    for slot in sched:
+        sc_lat.append(slot['lat'])
+        sc_long.append(slot['lng'])
+        sc_name.append(slot['name'])
+        sc_time.append(slot['time'])
+        sc_pid.append(slot['place_id'])
+    reg=args["reg"]
+    Bus(bid=LATEST_BID+1,reg=reg,sc_lat=sc_lat,sc_long=sc_long,sc_pid=sc_pid ,sc_name=sc_name,sc_time=sc_time).save()
+    LATEST_BID+=1
+    return make_response("",200)
+
+
+     
+
+    # return abort(401)
 
 if __name__ == '__main__':
     app.run(debug='True')
