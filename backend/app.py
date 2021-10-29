823a96d5822f4a70d5092f2ad848bb91698a25de
Neil Botelho <neil.botelho321@gmail.com>
2021-10-29 18:36:32 +0530

    Add failed app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,60 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request, jsonify
+from flask import g, request, abort
+from flask_mongoengine import MongoEngine
+from flask_mongoengine.wtf import model_form
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+import jwt
+import json
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# MongoDB setup
+app = Flask(__name__)
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'your_database',
+    'host': 'localhost',
+    'port': 27017
+}
+db = MongoEngine()
+db.init_app(app)
+
+#Models
+class BusManagerPrototype(db.Document):
+    phone_no = db.IntField()
+    password= db.StringField()
+
+    def to_json(self):
+        return {"number": self.phone_no,
+                "password": self.password}
+
+BusManager=model_form(BusManagerPrototype,field_args={"password":True})
+
+
+
+@app.route('/authenticate', methods=['POST'])
+def query_records():
+    phone_no = request.args.get('phone')
+    password= request.args.get("password")
+    user = BusManager.objects(phone_no=phone_no).first()
+    if not user:
+        return jsonify({'error': 'data not found'})
+    else:
+        # Passwrod verifaction
+        login_user(user)
+        return jsonify(user.to_json())
+
+@app.route('/', methods=['PUT'])
+def create_record():
+    record = json.loads(request.data)
+    user = User(name=record['name'],
+                email=record['email'])
+    user.save()
+    return jsonify(user.to_json())
+
+@app.route('/', methods=['POST'])
+def update_record():
+    record = json.loads(request.data)
+    user = User.objects(name=record['name']).first()
+    if not user:
+        return jsonify({'error': 'data not found'})
+    else:
+        user.update(email=record['email'])
+    return jsonify(user.to_json())
