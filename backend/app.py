59bd6e4b6a14596861a660adcb87874fe0e6a3b7
RudrakshK <stark.rudraksh@gmail.com>
2021-10-29 17:54:18 +0530

    Updated config for db

--- backend/app.py
+++ backend/app.py
@@ -4,9 +4,10 @@ from creds import mongodb_password
 
 app = Flask(__name__)
 
-database_name = bowner
-DB_URI = "mongodb+srv://dbUser:{}@bustracker.awm8x.mongodb.net/?retryWrites=true&w=majority".format(mongodb_password)
-app.config["MONGODB_HOST"] = DB_URI
+database_name = bustracker
+DB_URI = "mongodb+srv://dbUser:{}@bustracker.awm8x.mongodb.net/bustracker?retryWrites=true&w=majority".format(mongodb_password)
+app.config["MONGODB_HOST"] = DB_URI 
+
 
 db = MongoEngine()
 db.init_app(app)
