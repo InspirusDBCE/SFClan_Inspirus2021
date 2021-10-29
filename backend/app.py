27311c74c1e46e529ebdd02ea8c6b13c89dd56f4
RudrakshK <stark.rudraksh@gmail.com>
2021-10-30 01:22:57 +0530

    phone_no to phone

--- backend/app.py
+++ backend/app.py
@@ -33,11 +33,11 @@ bcrypt = Bcrypt(app)
 #Models
 class BusManager(db.Document):
     manager_id= db.StringField()
-    phone_no = db.IntField()
+    phone = db.IntField()
     password= db.StringField()
     busIds= db.ListField(db.IntField())
     def to_json(self):
-        return {"number": self.phone_no,
+        return {"number": self.phone,
                 "password": self.password,
                 "manager_id": self.id
                 }
@@ -49,8 +49,8 @@ def generate_random_string():
 def password_hash(password):
     return bcrypt.hashpw(password.encode("utf-8"),CANDIDATE)
 
-# BusManager(phone_no=123,password="alsdkjas",busIds=[12]).save()
-#BusManager(phone_no=4,password = "alsdkjas",manager_id="123",busIds=[12,13,14,15]).save()
+# BusManager(phone=123,password="alsdkjas",busIds=[12]).save()
+#BusManager(phone=4,password = "alsdkjas",manager_id="123",busIds=[12,13,14,15]).save()
 
 app.config['SECRET_KEY'] = 'phonixlodu'
 
@@ -66,7 +66,7 @@ def token_required(f):
         try:
             data = jwt.decode(token, app.config['SECRET_KEY'], algorithms = 'HS256')
             print(data)
-            BusMan = BusManager.objects(phone_no = data['phone_no']).first()
+            BusMan = BusManager.objects(phone = data['phone']).first()
         except Exception as e:
             print (e)
             return jsonify({'message':'Token is invalid'})
@@ -77,18 +77,15 @@ def token_required(f):
 #Login
 @app.route('/login',methods=['POST'])
 def query_logins():
-    phone_no = request.get_json().get("phone_no")
+    phone = request.get_json().get("phone")
     password = request.get_json().get("password")
     print(request.get_json())
-    obj = BusManager.objects(phone_no = phone_no)
+    obj = BusManager.objects(phone = phone)
     print(password, obj.first().to_json()['password'])
-    #print(obj.first.password, password)
-    #return obj
+
     if obj and password == obj.first().password:
-        # resp=make_response('set cookie')
-        # resp.set_cookie(COOKIE_NAME,obj.id)
-        # return jsonify({"cookie":obj.id})
-        token = jwt.encode({'phone_no' : phone_no, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=60)}, app.config['SECRET_KEY'])
+
+        token = jwt.encode({'phone' : phone, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=60)}, app.config['SECRET_KEY'])
 
         return jsonify({'token' : token})
     else:
@@ -97,6 +94,8 @@ def query_logins():
 @app.route('/hello', methods = ['GET'])
 def okok():
     return 'hello'
+
+
 
 @app.route('/authentication',methods=['GET'])
 @token_required
